a3335e20d083afe1a5d79029e068657e0628a0b8
mesutbunaldi <mesutbunaldi@gmail.com>
2020-04-15 01:09:17 +0300

    commit init

--- lib/main.dart
+++ lib/main.dart
@@ -111,7 +111,7 @@ class _SignUpFormState extends State<SignUpForm> {
             color: Colors.blue,
             textColor: Colors.white,
             onPressed: _formProgress == 1 ? _showWelcomeScreen : null,
-            child: Text('Sign up basdasdro'),
+            child: Text('Sign up basasdasdasdro'),
           ),
         ],
       ),
